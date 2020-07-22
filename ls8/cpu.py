efa1f81fe27c0b3562fecc9c70e279d10fb3b0cf
Vrndavana <Vrmdavama.th@gmail.com>
2020-07-22 13:53:20 -0700

    Day 2 Mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,187 +1,118 @@
 """CPU functionality."""
-import sys
 
-HTL = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
+import sys
+# print(sys.argv)
+# sys.exit(0)
 
 class CPU:
+    """Main CPU class."""
+
     def __init__(self):
-        self.reg = [0] * 8
+        """Construct a new CPU."""
         self.ram = [0] * 256
         self.pc = 0
-        self.fl = 0
+        self.reg = [0] * 8
+        self.HLT = 0b00000001
+        self.MUL = 0b10100010
+        self.address = 0
 
     def load(self):
         """Load a program into memory."""
-        address = 0
+
+        with open(sys.argv[1]) as f:
+            for line in f:
+                try:
+                    line = line.strip().split("#",1)[0]
+                    # if line == '':
+                    #     continue
+                    line = int(line, 2)
+                    self.ram[self.address] = line
+                    self.address += 1
+                    # print(line)
+                except ValueError:
+                    pass
+
+        # address = 0
+
         # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == self.MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
+        #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+
+
+
+    def ram_read(self, address=None):
+        v = self.ram[address]
+        return v
+
+    def ram_write(self, value=None, address=None):
+        self.ram[address] = value
+        
+
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
+
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
+            #self.fl,
+            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
+
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-        print()
+
+        print()    
 
     def run(self):
         """Run the CPU."""
-
-        while True:
-            instruction = bin(self.ram_read(self.pc))
-
-            print('instruction: ', instruction[2:])
-
+        self.trace()
+        running = True
+        while running:
             IR = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if IR is HTL:
-                self.handle_halt()
-            elif IR is LDI:
-                self.handle_ldi(operand_a, operand_b)
-            elif IR is PRN:
-                self.handle_print(operand_a)
-
-            self.pc += 1
-
-    def handle_halt(self):
-        # cpu reset
-        for i in range(0, 6):
-            self.reg[i] = 0
-        self.reg[7], self.pc, self.fl, self.ram == 0xF4, 0, 0, 0
-
-        sys.exit(1)
-
-    def handle_print(self, index):
-        '''
-        print numeric value stored at given register
-        '''
-        print(self.reg[index])
-
-    def handle_ldi(self, register_index, value):
-        '''
-        set the value of a register to an integer
-        '''
-        if register_index < len(self.reg):
-            self.reg[register_index] = value
-        else:
-            raise IndexError
-
-    def ram_read(self, MAR):
-        '''
-        Returns what is stored at the given address.
-        '''
-        try:
-            if MAR < len(self.ram):
-                return self.ram[MAR]
-        except IndexError:
-            return f'address is out of range.'
-
-    def ram_write(self, MAR, MDR):
-        '''
-        Writes the given value to the address given.
-        '''
-        try:
-            if MAR < len(self.ram):
-                self.ram[MAR] = MDR
-        except IndexError:
-            return f'address is out of range.'
-
-
-
-# 
-# 
-#  """CPU functionality."""
-
-# import sys
-
-# class CPU:
-#     """Main CPU class."""
-
-#     def __init__(self):
-#         """Construct a new CPU."""
-#         pass
-
-#     def load(self):
-#         """Load a program into memory."""
-
-#         address = 0
-
-#         # For now, we've just hardcoded a program:
-
-#         program = [
-#             # From print8.ls8
-#             0b10000010, # LDI R0,8
-#             0b00000000,
-#             0b00001000,
-#             0b01000111, # PRN R0
-#             0b00000000,
-#             0b00000001, # HLT
-#         ]
-
-#         for instruction in program:
-#             self.ram[address] = instruction
-#             address += 1
-
-
-#     def alu(self, op, reg_a, reg_b):
-#         """ALU operations."""
-
-#         if op == "ADD":
-#             self.reg[reg_a] += self.reg[reg_b]
-#         #elif op == "SUB": etc
-#         else:
-#             raise Exception("Unsupported ALU operation")
-
-#     def trace(self):
-#         """
-#         Handy function to print out the CPU state. You might want to call this
-#         from run() if you need help debugging.
-#         """
-
-#         print(f"TRACE: %02X | %02X %02X %02X |" % (
-#             self.pc,
-#             #self.fl,
-#             #self.ie,
-#             self.ram_read(self.pc),
-#             self.ram_read(self.pc + 1),
-#             self.ram_read(self.pc + 2)
-#         ), end='')
-
-#         for i in range(8):
-#             print(" %02X" % self.reg[i], end='')
-
-#         print()
-
-#     def run(self):
-#         """Run the CPU."""
-#         pass
+            if IR ==  0b10000010:  # LDI instruction
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == 0b01000111:  # PRN instruction
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == self.HLT:
+                running = False
+            elif IR == self.MUL:
+                # self.reg[operand_a] *= self.reg[operand_b]
+                self.alu(IR, operand_a, operand_b)
+                self.pc += 3
+            else:
+                print(f"Unknown instruction {IR}")
+                running = False  
